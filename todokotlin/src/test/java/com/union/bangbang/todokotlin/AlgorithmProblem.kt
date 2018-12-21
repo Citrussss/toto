d88e778655130783bdda32fdb19dbd70013cf3ca
jimby <huangxiujie@email.com>
2018-12-21 11:53:05 +0800

    :twisted_rightwards_arrows: 合并分支

---
+++ todokotlin/src/test/java/com/union/bangbang/todokotlin/AlgorithmProblem.kt
@@ -0,0 +1,16 @@
+package com.union.bangbang.todokotlin
+
+import io.reactivex.Observable
+import io.reactivex.Observer
+import io.reactivex.functions.Predicate
+import org.junit.Test
+import java.util.*
+
+class AlgorithmProblem {
+    var string = arrayOf("dasdas", "dasdasda")
+    @Test
+    fun math(): Unit {
+        val map = string.filter { it.length == 5 }.sortedBy { it }.map { it.toUpperCase() }.toList()
+        Observable.fromIterable(map).filter { it.length == 5 }.subscribe{t: String? ->  }
+    }
+}
