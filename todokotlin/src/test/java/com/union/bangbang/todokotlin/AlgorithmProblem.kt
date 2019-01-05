e3ff86cef3036204b206c8cd3ead9656f76cb49b
bangbang <292837678@qq.com>
2019-01-05 15:15:47 +0800

    备份

--- todokotlin/src/test/java/com/union/bangbang/todokotlin/AlgorithmProblem.kt
+++ todokotlin/src/test/java/com/union/bangbang/todokotlin/AlgorithmProblem.kt
@@ -5,6 +5,7 @@ import io.reactivex.Observer
 import io.reactivex.functions.Predicate
 import org.junit.Test
 import java.util.*
+import kotlin.collections.ArrayList
 
 class AlgorithmProblem {
     var string = arrayOf("dasdas", "dasdasda")
@@ -12,5 +13,10 @@ class AlgorithmProblem {
     fun math(): Unit {
         val map = string.filter { it.length == 5 }.sortedBy { it }.map { it.toUpperCase() }.toList()
         Observable.fromIterable(map).filter { it.length == 5 }.subscribe{t: String? ->  }
+
+        var list:ArrayList<String> = ArrayList<String>()
+        list.removeAt(1)
     }
+
+
 }
